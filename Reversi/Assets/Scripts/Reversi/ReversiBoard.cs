98db284a9f6894a5d4b2511c462f2c32ef69b9d6
mc-hattori <hattori@m-craft.com>
2023-10-04 15:17:04 +0900

    配置, ゲーム終了判定、パス、やり直しを行うメソッドをボードクラスに追加。 石の色を表す列挙体に、反転色を取得するための拡張メソッドを追加。

--- Reversi/Assets/Scripts/Reversi/ReversiBoard.cs
+++ Reversi/Assets/Scripts/Reversi/ReversiBoard.cs
@@ -1,48 +1,180 @@
-﻿using NUnit.Framework.Constraints;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Reversi
 {
     [System.Serializable]
-
-
     public class Board
     {
 
         // --- private ---
         // enum
+        [System.Flags]
         private enum Direction
         {
-            None = 0,
-            Upper = 1,
-            UpperLeft = 2,
-            Left = 4,
-            LowerLeft = 8,
-            Lower = 16,
-            LowerRight = 32,
-            Right = 64,
-            UpperRight = 128,
+            None        = 0,
+            Upper       = 1,
+            UpperLeft   = 1 << 1,
+            Left        = 1 << 2,
+            LowerLeft   = 1 << 3,
+            Lower       = 1 << 4,
+            LowerRight  = 1 << 5,
+            Right       = 1 << 6,
+            UpperRight  = 1 << 7,
         }
 
         // variable
         private DiscColor[,] rawBoard = new DiscColor[Constant.BoardSize + 2, Constant.BoardSize + 2];
 
-        private uint turn;
+        private int turn;
         private DiscColor currentColor;
 
         private List<List<Disc>> updatedDiscList = new List<List<Disc>>();
 
         [SerializeField]
-        private List<Point> movablePointList = new List<Point>(Constant.MaxTurn + 1);
-        private uint[,,] moveableDirection = new uint[Constant.MaxTurn,Constant.BoardSize+2,Constant.BoardSize+2];
-        DiscColorStorage<uint> discList = new DiscColorStorage<uint>();
+        private List<Point>[] movablePointList = new List<Point>[Constant.BoardSize + 1];
+        private Direction[,,] movableDirection = new Direction[Constant.MaxTurn,Constant.BoardSize+2,Constant.BoardSize+2];
+        DiscColorStorage<int> discList = new DiscColorStorage<int>();
 
 
         // method
+
+        /// <summary>
+        /// point で指定された座標に石をうち、挟み込むことができるすべての石を裏返す。<br/>
+        /// 「打った石」と「裏返した石」を updatedDiscList に追加する。
+        /// </summary>
+        /// <param name="point">石を打つ座標</param>
         private void FlipDiscs(in Point point)
         {
+            int x,y;
+            Disc operation = new Disc(point.x,point.y,currentColor);
 
+            Direction dir = movableDirection[turn,point.x,point.y];
+            
+            List<Disc> update = new List<Disc>();
+
+            rawBoard[point.x,point.y] = currentColor;
+            update.Add(operation);
+
+            // 上に置ける場合
+            if(dir.HasFlag(Direction.Upper))
+            {
+                y = point.y;
+                operation.x = point.x;
+                while(rawBoard[point.x,--y] != currentColor)
+                {
+                    rawBoard[point.x,y] = currentColor;
+                    operation.y = y;
+                    update.Add(operation);
+                }
+            }
+
+            // 下に置ける場合
+            if(dir.HasFlag(Direction.Lower))
+            {
+                y = point.y;
+                operation.x = point.x;
+                while(rawBoard[point.x,++y] != currentColor)
+                {
+                    rawBoard[point.x,y] = currentColor;
+                    operation.y = y;
+                    update.Add(operation);
+                }
+            }
+
+            // 左に置ける場合
+            if(dir.HasFlag(Direction.Left))
+            {
+                x = point.x;
+                operation.y = point.y;
+                while(rawBoard[--x,point.y] != currentColor)
+                {
+                    rawBoard[x,point.y] = currentColor;
+                    operation.x = x;
+                    update.Add(operation);
+                }
+            }
+
+            // 右に置ける場合
+            if(dir.HasFlag(Direction.Right))
+            {
+                x = point.x;
+                operation.y = point.y;
+                while(rawBoard[++x,point.y] != currentColor)
+                {
+                    rawBoard[x,point.y] = currentColor;
+                    operation.x = x;
+                    update.Add(operation);
+                }
+            }
+
+            // 右上に置ける場合
+            if(dir.HasFlag(Direction.UpperRight))
+            {
+                x = point.x;
+                y = point.y;
+                while(rawBoard[++x,--y] != currentColor)
+                {
+                    rawBoard[x,y] = currentColor;
+                    operation.x = x;
+                    operation.y = y;
+                    update.Add(operation);
+                }
+            }
+
+            // 左上に置ける場合
+            if(dir.HasFlag(Direction.UpperLeft))
+            {
+                x = point.x;
+                y = point.y;
+                while(rawBoard[--x,--y] != currentColor)
+                {
+                    rawBoard[x,y] = currentColor;
+                    operation.x = x;
+                    operation.y = y;
+                    update.Add(operation);
+                }
+            }
+
+            // 左に置ける場合
+            if(dir.HasFlag(Direction.LowerLeft))
+            {
+                x = point.x;
+                y = point.y;
+                while(rawBoard[--x,++y] != currentColor)
+                {
+                    rawBoard[x,y] = currentColor;
+                    operation.x = x;
+                    operation.y = y;
+                    update.Add(operation);
+                }
+            }
+
+            // 右に置ける場合
+            if(dir.HasFlag(Direction.LowerRight))
+            {
+                x = point.x;
+                y = point.y;
+                while(rawBoard[++x,++y] != currentColor)
+                {
+                    rawBoard[x,y] = currentColor;
+                    operation.x = x;
+                    operation.y = y;
+                    update.Add(operation);
+                }
+            }
+
+
+            // 石の数を更新、反映
+            int discdiff = update.Count;
+
+            discList[currentColor] += discdiff;
+            discList[currentColor.Inverted()] -= discdiff - 1;
+            discList[DiscColor.Empty]--;
+
+            updatedDiscList.Add(update);
         }
 
         /// <summary>
@@ -52,94 +184,118 @@ namespace Reversi
         /// <param name="disc">指定する座標</param>
         /// <returns>石を裏返せる方向にフラグが立った整数値を返す</returns>
         /// <seealso cref="Reversi.Board.Direction"/>
-        private uint CheckMobility(in Disc disc)
+        private Direction CheckMobility(in Disc disc)
         {
             // 既に石が置かれていたら置けない
             // not placeable if a disc is already placed
-            if (rawBoard[disc.x, disc.y] != DiscColor.Empty) return ((uint)Direction.None);
+            if (rawBoard[disc.x, disc.y] != DiscColor.Empty) return Direction.None;
 
             int x, y;
-            uint dir = (uint)Direction.None;
+            Direction dir = Direction.None;
 
             // 上 - Upper
-            if ((int)rawBoard[disc.x,disc.y - 1] == -(int)disc.color)
+            if (rawBoard[disc.x,disc.y - 1] == disc.color.Inverted())
             {
                 x = disc.x;
                 y = disc.y - 2;
-                while ((int)rawBoard[x,y] == -(int)disc.color) { y--; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.Upper;
+                while (rawBoard[x,y] == disc.color.Inverted()) { y--; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.Upper;
             }
 
             // 下 - Lower
-            if ((int)rawBoard[disc.x,disc.y + 1] == -(int)disc.color)
+            if (rawBoard[disc.x,disc.y + 1] == disc.color.Inverted())
             {
                 x = disc.x;
                 y = disc.y + 2;
-                while ((int)rawBoard[x,y] == -(int)disc.color) { y++; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.Lower;
+                while (rawBoard[x,y] == disc.color.Inverted()) { y++; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.Lower;
             }
 
             // 左 - Left
-            if ((int)rawBoard[disc.x - 1, disc.y] == -(int)disc.color)
+            if (rawBoard[disc.x - 1, disc.y] == disc.color.Inverted())
             {
                 x = disc.x - 2;
                 y = disc.y;
-                while ((int)rawBoard[x, y] == -(int)disc.color) { x--; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.Left;
+                while (rawBoard[x, y] == disc.color.Inverted()) { x--; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.Left;
             }
 
             // 右 - Right
-            if ((int)rawBoard[disc.x + 1, disc.y] == -(int)disc.color)
+            if (rawBoard[disc.x + 1, disc.y] == disc.color.Inverted())
             {
                 x = disc.x + 2;
                 y = disc.y;
-                while ((int)rawBoard[x, y] == -(int)disc.color) { x++; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.Right;
+                while (rawBoard[x, y] == disc.color.Inverted()) { x++; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.Right;
             }
 
             // 右上 - UpperRight
-            if ((int)rawBoard[disc.x + 1, disc.y - 1] == -(int)disc.color)
+            if (rawBoard[disc.x + 1, disc.y - 1] == disc.color.Inverted())
             {
                 x = disc.x + 2;
                 y = disc.y - 2;
-                while ((int)rawBoard[x, y] == -(int)disc.color) { x++; y--; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.UpperRight;
+                while (rawBoard[x, y] == disc.color.Inverted()) { x++; y--; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.UpperRight;
             }
 
             // 左上 - UpperLeft
-            if ((int)rawBoard[disc.x - 1, disc.y - 1] == -(int)disc.color)
+            if (rawBoard[disc.x - 1, disc.y - 1] == disc.color.Inverted())
             {
                 x = disc.x - 2;
                 y = disc.y - 2;
-                while ((int)rawBoard[x, y] == -(int)disc.color) { x--; y--; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.UpperLeft;
+                while (rawBoard[x, y] == disc.color.Inverted()) { x--; y--; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.UpperLeft;
             }
 
             // 左下 - LowerLeft
-            if ((int)rawBoard[disc.x - 1, disc.y + 1] == -(int)disc.color)
+            if (rawBoard[disc.x - 1, disc.y + 1] == disc.color.Inverted())
             {
                 x = disc.x - 2;
                 y = disc.y + 2;
-                while ((int)rawBoard[x, y] == -(int)disc.color) { x--; y++; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.LowerLeft;
+                while (rawBoard[x, y] == disc.color.Inverted()) { x--; y++; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.LowerLeft;
             }
 
 
             // 右下 - LowerRight
-            if ((int)rawBoard[disc.x + 1, disc.y + 1] == -(int)disc.color)
+            if (rawBoard[disc.x + 1, disc.y + 1] == disc.color.Inverted())
             {
                 x = disc.x + 2;
                 y = disc.y + 2;
-                while ((int)rawBoard[x, y] == -(int)disc.color) { x++; y++; }
-                if ((int)rawBoard[x, y] == (int)disc.color) dir |= (uint)Direction.LowerRight;
+                while (rawBoard[x, y] == disc.color.Inverted()) { x++; y++; }
+                if (rawBoard[x, y] == disc.color) dir |= Direction.LowerRight;
             }
 
             return dir;
         }
 
+        /// <summary>
+        /// 現在の手番における着手可能な手を調べなおす。<br/>
+        /// movablePoint, movableDirection を更新する。
+        /// </summary>
         private void InitMovable()
         {
+            Disc disc = new Disc(0,0,currentColor);
+                
+            Direction dir;
+            movablePointList[turn].Clear();
 
+            for(int x = 1; x <= Constant.BoardSize; x++)
+            {
+                disc.x = x;
+                for(int y = 1; y <= Constant.BoardSize; y++)
+                {
+                    disc.y = y;
+
+                    dir = CheckMobility(disc);
+                    if(dir != Direction.None)
+                    {
+                        // 配置可能な場所に追加
+                        movablePointList[turn].Add(disc);
+                    }
+                    movableDirection[turn,x,y] = dir;
+                }
+            }
         }
 
 
@@ -160,33 +316,122 @@ namespace Reversi
         /// </returns>
         public bool Move(in Point point)
         {
+            // 石が打てる位置かどうかを判定する
+            // 打てない位置ならfalseで処理抜け
+            // 座標の値が正しい範囲かどうかもここでチェック
             if(point.x < 0 || point.x >= Constant.BoardSize) return false;
             if(point.y < 0 || point.y >= Constant.BoardSize) return false;
-            if(moveableDirection[turn,point.x,point.y] == (uint)Direction.None) return false;
+            if(movableDirection[turn,point.x,point.y] == (uint)Direction.None) return false;
 
+            // 石を返す
             FlipDiscs(point);
 
+            // 手番の色や現在の手数などを更新
             turn++;
-            (uint)currentColor = -(uint)currentColor;
+            currentColor = (DiscColor)System.Enum.ToObject(typeof(DiscColor), -(uint)currentColor);
+
+            // movableDir, movablePointを調べなおす
+            InitMovable();
+
+            return true;
+        }
+
+        /// <summary>
+        /// パスを試み、その結果を返す。
+        /// </summary>
+        /// <returns>成功すればtrue、失敗したらfalseを返す。</returns>
+        public bool Pass()
+        {
+            // 打つ手があれば、パスはできない
+            if(movablePointList[turn].Count != 0) return false;
+
+            // ゲームが終了しているなら、パスはできない
+            if(IsGameOver()) return false;
+            currentColor = currentColor.Inverted();
+
+            // 空の更新情報を追加
+            updatedDiscList.Add(new List<Disc>());
 
             InitMovable();
 
             return false;
         }
 
-        public bool Pass()
-        {
-            return false;
-        }
-
+        /// <summary>
+        /// 直前の一手を元に戻すことを試み、その結果を返す。
+        /// </summary>
+        /// <returns>成功するとtrue、元に戻せない場合はfalseを返す</returns>
         public bool Undo()
         {
-            return false;
+            if(turn == 0) return false;
+
+            currentColor = currentColor.Inverted();
+
+            List<Disc> update = updatedDiscList[updatedDiscList.Count - 1];
+
+            if(update.Count == 0)   // 前回がパスの場合
+            {
+                // movablePointListとmovableDirectionを再構築
+                movablePointList[turn].Clear();
+                for(int x = 1;x <= Constant.BoardSize;x++)
+                {
+                    for(int y = 1;y <= Constant.BoardSize;y++)
+                    {
+                        movableDirection[turn,x,y] = Direction.None;
+                    }
+                }
+            }
+            else    // 前回がパスでない
+            {
+                turn--;
+
+                // 石を元に戻す
+                rawBoard[update[0].x,update[0].y] = DiscColor.Empty;
+                for(int i = 1; i < update.Count; i++)
+                {
+                    rawBoard[update[i].x,update[i].y] = currentColor.Inverted();
+                }
+
+                // 石数の更新
+                int discdiff = update.Count;
+                discList[currentColor] -= discdiff;
+                discList[currentColor.Inverted()] += discdiff - 1;
+                discList[DiscColor.Empty]++;
+            }
+
+            // 不要になったupdateを1つ削除
+            updatedDiscList.RemoveAt(updatedDiscList.Count - 1);
+
+            return true;
         }
 
+        /// <summary>
+        /// ゲームが終了しているかどうかを返す。
+        /// </summary>
+        /// <returns>終了していればtrue, 終了していなければfalseを返す。</returns>
         public bool IsGameOver()
         {
-            return false;
+            // 60手に達していたらゲーム終了
+            if(turn == Constant.MaxTurn) return true;
+
+            // 打てる手があればゲーム終了ではない
+            if(movablePointList[turn].Count != 0) return false;
+
+            // 現在の手番と逆の色が打てるかどうか調べる
+            Disc disc = new Disc();
+            disc.color = currentColor.Inverted();
+            for(int x = 1; x <= Constant.BoardSize; x++)
+            {
+                disc.x = x;
+                for(int y = 1; y <= Constant.BoardSize; y++)
+                {
+                    disc.y = y;
+                    // 置ける場所が1つでもある場合、ゲーム終了ではない
+                    if(CheckMobility(disc) != Direction.None) return false;
+                }
+            }
+
+            return true;
         }
 
         public uint CountDisc(DiscColor color)
@@ -199,9 +444,9 @@ namespace Reversi
             return DiscColor.Empty;
         }
 
-        public void GetMovablePoint(out List<Point> moveablePointList)
+        public void GetMovablePoint(out List<Point> movablePointList)
         {
-            moveablePointList = new List<Point>();
+            movablePointList = new List<Point>();
         }
 
         public List<Disc> GetUpdate()
--- Reversi/Assets/Scripts/Reversi/ReversiColorEnum.cs
+++ Reversi/Assets/Scripts/Reversi/ReversiColorEnum.cs
@@ -10,5 +10,29 @@
         Black = 1,
         Wall = 2
     }
+
+    public static class DiscColorUtil
+    {
+        // extension method
+
+        /// <summary>
+        /// 代入された石色と反対の石色を取得する。<br/>
+        /// 空や、壁の場合はそのまま返す。
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static DiscColor Inverted(this DiscColor color)
+        {
+            switch(color)
+            {
+                case DiscColor.White:
+                return DiscColor.Black;
+                case DiscColor.Black:
+                return DiscColor.White;
+                default:
+                return color;
+            }
+        }
+    }
 }
 
