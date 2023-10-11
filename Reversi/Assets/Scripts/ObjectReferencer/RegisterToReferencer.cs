7c3bf7d82c97f1c24b362b5bc7f27c3f5483821c
mc-hattori <hattori@m-craft.com>
2023-10-11 11:35:22 +0900

    オブジェクト参照保持用のスクリプトにアセット作成用関数を追加。

--- Reversi/Assets/Scripts/ObjectReferencer/RegisterToReferencer.cs
+++ Reversi/Assets/Scripts/ObjectReferencer/RegisterToReferencer.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEditor;
+using System.IO;
+using UnityEngine.Serialization;
 
 namespace RegisterObject
 {
@@ -9,16 +11,110 @@ namespace RegisterObject
 	[DefaultExecutionOrder(-100)]
 	public class RegisterToReferencer : MonoBehaviour 
 	{
+		/// <summary>
+		/// アセット生成用の設定を定義するためのクラス
+		/// </summary>
+		[System.Serializable]
+		public class AssetCreationSettings
+		{
+			/// <summary>
+			/// 定数：アセット生成デフォルトパス
+			/// </summary>
+			public const string DefaultPath = "ScriptableObjects/ObjectReferencer/";
+
+			/// <summary>
+			/// 定数：アセットデフォルト接頭辞（ファイル名先頭に付加）
+			/// </summary>
+			public const string DefaultPrefix = "Ref_";
+
+			/// <summary>
+			/// アセットを生成する際のパス
+			/// </summary>
+			[Header("アセット生成先のパス")]
+			public string destPath = DefaultPath;
+
+			/// <summary>
+			/// アセットを生成する際のパス
+			/// </summary>
+			[Header("アセット生成時の接頭辞（ファイル名先頭に付加）")]
+			public string prefix = DefaultPrefix;
+
+			/// <summary>
+			/// デフォルト設定に戻す
+			/// </summary>
+			public void RestoreDefault()
+			{
+				destPath = DefaultPath;
+				prefix = DefaultPrefix;
+			}
+		}
+		
+		/// <summary>
+		/// 登録後に非Activeにする
+		/// </summary>
 		[SerializeField,Header("受け渡し後に非Activeにする")]
-		private bool deactivateAfterRegister = false;
+		private bool _deactivateAfterRegister = false;
 
+		/// <summary>
+		/// 登録先のObjectReferencer
+		/// </summary>
 		[SerializeField,Header("対象となる媒介用ScriptableObject")]
-        private ObjectReferencer relay;
+        private ObjectReferencer _referencer;
 
+		/// <summary>
+		/// アセット生成時の設定を定義
+		/// </summary>
+		[SerializeField,Header("アセット生成用設定")]
+		private AssetCreationSettings _assetCreation;
+
+		/// <summary>
+		/// このオブジェクトがアクティブになった際にコールされる関数
+		/// セットされているObject Referencerにオブジェクトを登録する
+		/// </summary>
 		private void Awake()
 		{
-			relay.gameObject = gameObject;
-			if(deactivateAfterRegister) gameObject.SetActive(false);
+			if(_referencer)
+			{
+				_referencer.gameObject = gameObject;
+				if(_deactivateAfterRegister) gameObject.SetActive(false);
+			}
+			else
+			{
+				Debug.LogError($"There is no referencer to register!");
+			}
 		}
+
+		/// <summary>
+		/// アセットの生成用設定をデフォルトに設定しなおす
+		/// </summary>
+		[ContextMenu("Restore default asset creation path")]
+		public void RestoreDefaultCreationSettings()
+		{
+			_assetCreation.RestoreDefault();
+		}
+
+		/// <summary>
+		/// アセット生成パスに基づいてアタッチされたオブジェクトへの参照アセットを作成
+		/// </summary>
+		[ContextMenu("Create referencer asset to attached gameObject (OVERWRITE)")]
+		public void CreateReferencerAsset()
+		{
+			// ObjectReferencerインスタンス生成
+			var referencerInstance = ScriptableObject.CreateInstance<ObjectReferencer>();
+			referencerInstance.gameObject = this.gameObject;
+			string fileName = $"{_assetCreation.prefix}{this.gameObject.name}.asset";
+			string destPath = "Assets/" + _assetCreation.destPath;
+
+			// インスタンスを指定したディレクトリに保存
+			if (!Directory.Exists(destPath))
+				Directory.CreateDirectory(destPath);
+			AssetDatabase.CreateAsset(referencerInstance, destPath + fileName);
+
+			Debug.Log($"Created referencer asset at {destPath + fileName}.");
+
+			// 生成したインスタンスを登録先としてセット
+			_referencer = referencerInstance;
+		}
+
 	}
 }
