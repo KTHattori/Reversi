7e9bb0d3ec758f1afc9aa857f479c56facf5b16a
mc-hattori <hattori@m-craft.com>
2023-10-11 16:48:39 +0900

    コメント抜けがあったので追加修正しました。

--- Reversi/Assets/Scripts/Reversi/Class/ReversiConstant.cs
+++ Reversi/Assets/Scripts/Reversi/Class/ReversiConstant.cs
@@ -5,7 +5,13 @@
     /// </summary>
     public static class Constant
     {
+        /// <summary>
+        /// ボードの1辺サイズ
+        /// </summary>
         public static readonly int BoardSize = 8;
+        /// <summary>
+        /// 最大ターン数（マス目総数）
+        /// </summary>
         public static readonly int MaxTurn = 60;
     }
 }
