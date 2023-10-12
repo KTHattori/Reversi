8d011fde8b9efb4387f5bf5ebee0162e59c594f3
mc-hattori <hattori@m-craft.com>
2023-10-12 18:17:25 +0900

    プレイヤー用インタフェースの定義

---
+++ Reversi/Assets/Scripts/Reversi/Class/ReversiIPlayer.cs
@@ -0,0 +1,14 @@
+namespace Reversi
+{
+    /// <summary>
+    /// リバーシの手を打つことができるクラスがもつインタフェース
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// 自分の手番が来た時の行動内容。
+        /// </summary>
+        /// <param name="board"></param>
+        public abstract void OnTurn(in Board board);
+    }
+}
